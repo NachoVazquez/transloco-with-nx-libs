8619287db53de1749ac951ed676d1d29b4906955
Nacho Vazquez <nachovc1410@gmail.com>
2020-02-29 15:59:41 -0300

    fix aot issues

--- libs/transloco-lib-b/src/lib/transloco-lib-b.module.ts
+++ libs/transloco-lib-b/src/lib/transloco-lib-b.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocationBComponent } from './location-b.component';
 
-const loader = ['en'].reduce((acc: any, lang: string) => {
-  acc[lang] = () => import(`../assets/i18n/${lang}.json`);
-  return acc;
-}, {});
+// const loader = ['en'].reduce((acc: any, lang: string) => {
+//   acc[lang] = () => import(`../assets/i18n/${lang}.json`);
+//   return acc;
+// }, {});
+
+export function en() {
+  return import(`../assets/i18n/en.json`);
+}
+
+export function es() {
+  return import(`../assets/i18n/es.json`);
+}
 
 @NgModule({
-  imports: [CommonModule, TranslocoConfigModule.forChild('libB', loader)],
+  imports: [CommonModule, TranslocoConfigModule.forChild('libB', {en, es})],
   declarations: [LocationBComponent],
   providers: [],
   exports: [LocationBComponent]
